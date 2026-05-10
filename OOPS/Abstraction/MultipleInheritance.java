77b7d6a78094a051fd1a6f44effee1db45d7baf9
BhaveshDotCom <mail.bhavesh.in@gmail.com>
2026-05-10 17:30:54 +0530

    feat(abstraction): implement MultipleInheritance

---
+++ OOPS/Abstraction/MultipleInheritance.java
@@ -0,0 +1,29 @@
+package OOPS.Abstraction;
+
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        Smartphone smartphone = new Smartphone(); 
+        smartphone.clickPhoto();
+        smartphone.playMusic();
+    }
+
+}
+
+interface Camera {
+    void clickPhoto();
+}
+
+interface MusicPlayer {
+    void playMusic();
+}
+
+class Smartphone implements Camera, MusicPlayer {
+
+    public void clickPhoto(){
+        System.out.println("Say cheese!");
+    }
+
+    public void playMusic(){
+        System.out.println("Check out this track");
+    }
+}
