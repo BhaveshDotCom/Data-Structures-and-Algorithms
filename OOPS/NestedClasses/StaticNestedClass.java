c2d4f57581e62db63de428412ee0ab36297ff9c7
BhaveshDotCom <mail.bhavesh.in@gmail.com>
2026-05-16 20:12:27 +0530

    improve encapsulation and add bank account

--- OOPS/NestedClasses/StaticNestedClass.java
+++ OOPS/NestedClasses/StaticNestedClass.java
@@ -3,30 +3,52 @@ package OOPS.NestedClasses;
 
 public class StaticNestedClass {
     public static void main(String[] args) {
+
+        // Concept
         OuterClass myOuterClass = new OuterClass();
         OuterClass.InnerClass myClass = new OuterClass.InnerClass(myOuterClass, "Bhavesh");
         myClass.display();
+
+        // Bank
+        BankAccount myBankAccount = new BankAccount();
+        System.out.println(myBankAccount.computeSimpleInterest(10000000.0, 5));
     }
 
 }
 
+// Concept
 class OuterClass {
     private static int age = 19;
-    String role = "Engineer";
+    private String role = "Engineer";
 
     static class InnerClass {
-        OuterClass OuterClass;
-        String name;
+        OuterClass outerClass;
+        private String name;
 
-        InnerClass(OuterClass OuterClass,String name){
+        InnerClass(OuterClass outerClass,String name){
             this.name = name;
-            this.OuterClass = OuterClass;
+            this.outerClass = outerClass;
         }
 
         void display(){
             System.out.println("Name: "+name);
             System.out.println("Age: "+age);
-            System.out.println("Role: "+OuterClass.role);
+            System.out.println("Role: " + outerClass.role); // non-static member of the OuterClass
         }
+    }
+}
+
+// Bank
+class BankAccount {
+
+    // Helper Class
+    private static class InterestCalculator {
+        static double simpleInterest(double principalAmount, int timeInYears, float rate){
+            return (principalAmount*rate*timeInYears)/100;
+        }  
+    }
+        
+    public double computeSimpleInterest(double principal, int timeInYears){
+        return InterestCalculator.simpleInterest(principal, timeInYears, 9.0f);
     }
 }
