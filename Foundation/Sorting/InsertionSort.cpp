cd5bc72e7f4f5fd75974907d1e8c8c613b7bccec
BhaveshDotCom <mail.bhavesh.in@gmail.com>
2026-07-04 18:59:53 +0530

    insertion sort

---
+++ Foundation/Sorting/InsertionSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertion_sort_for_loop(vector<int> &arr){
+
+    for(int i=0; i<arr.size(); i++){
+        for(int j=i; j>0; j--){
+            if(arr[j]<arr[j-1]){
+                swap(arr[j], arr[j-1]);
+            }
+            else{
+                break;
+            }
+        }
+    }
+}
+
+void insertion_sort_while_loop(vector<int> &arr){
+    for(int i=0; i< arr.size(); i++){
+        int j=i;
+        while(j>0 && arr[j]<arr[j-1]){
+            swap(arr[j], arr[j-1]);
+            j--;
+        }
+    }
+}
+
+
+
+int main(){  
+
+    vector<int> arr = {1,4,2,6,3,9,6};
+    // insertion_sort_for_loop(arr);
+    insertion_sort_while_loop(arr);
+
+    for(int ele : arr){
+        cout << ele << " ";
+    }
+
+    return 0;
+}
